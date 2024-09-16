08b854a8650f9994b0e673d4aab7430a43162bdf
Aligh979 <89577061+Aligh979@users.noreply.github.com>
2024-09-16 02:38:33 +0200

    Table component

--- in5320-assignment2-master/src/Table.js
+++ in5320-assignment2-master/src/Table.js
@@ -2,13 +2,32 @@ function Table(props) {
   console.log(props.apiData);
 
   if (!props.apiData.results) {
-    // If the API request isn't completed return "loading...""
+    // If the API request isn't completed return "loading..."
     return <p>Loading...</p>;
   } else {
-    // Write your code here:
-    return <table>
-
-    </table>;
+    // Create table headers
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Country</th>
+            <th>Continent</th>
+            <th>Population</th>
+            <th>Population Growth</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.apiData.results.map((country, index) => (
+            <tr key={index}>
+              <td>{country.Country}</td>
+              <td>{country.Continent}</td>
+              <td>{country.Population}</td>
+              <td>{country.PopulationGrowth}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
   }
 }
 
